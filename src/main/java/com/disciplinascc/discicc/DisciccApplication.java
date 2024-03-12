4ebc4a70739b28ba4a7894c249654a3a3abcd3a9
VimersonSilva <vimerson.silva@ccc.ufcg.edu.br>
2024-03-12 00:29:24 -0300

    Project created

---
+++ src/main/java/com/disciplinascc/discicc/DisciccApplication.java
@@ -0,0 +1,13 @@
+package com.disciplinascc.discicc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisciccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisciccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/disciplinascc/discicc/DisciccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disciplinascc.discicc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisciccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
