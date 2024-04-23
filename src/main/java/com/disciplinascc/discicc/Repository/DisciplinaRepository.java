963ca7e29d351521cb4007bed326a93923f5a300
VimersonSilva <vimerson.silva@ccc.ufcg.edu.br>
2024-04-23 01:49:58 -0300

    Tudo funcionando perfeitamente

--- src/main/java/com/disciplinascc/discicc/Repository/DisciplinaRepository.java
+++ src/main/java/com/disciplinascc/discicc/Repository/DisciplinaRepository.java
@@ -10,8 +10,8 @@ import java.util.List;
 public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
 
     @Query(nativeQuery = true, value = """
-            SELECT tb_disciplina.id, tb_disciplina.codigo_disciplina, tb_disciplina.nome_disciplina,
-             tb_disciplina.num_periodo, tb_disciplina.quant_creditos, tb_disciplina.sigla, tb_belonging_periodo.position
+            SELECT tb_disciplina.id, tb_disciplina.codigo_disciplina AS codigoDisciplina, tb_disciplina.nome_disciplina AS nomeDisciplina,
+             tb_disciplina.num_periodo AS numPeriodo, tb_disciplina.quant_creditos AS quantCreditos, tb_disciplina.sigla, tb_belonging_periodo.position
              FROM tb_disciplina
              INNER JOIN tb_belonging_periodo ON tb_disciplina.id = tb_belonging_periodo.disciplina_id
              WHERE tb_belonging_periodo.list_id = :listId
